c2ce4c1c10a104507f20799746973a0cc8d02907
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2026-04-02 01:27:03 +0300

    test(cli): add -h and unknown option tests

--- spec/aicommit_spec.sh
+++ spec/aicommit_spec.sh
@@ -1,7 +1,20 @@
-Describe 'aicommit --help'
-  It 'displays help message'
+Describe 'aicommit'
+  It 'displays help message for -h'
     When call ./aicommit --help
     The status should be success
     The output should include 'Usage: aicommit [options]'
+  End
+
+  It 'displays help message for --help'
+    When call ./aicommit -h
+    The status should be success
+    The output should include 'Usage: aicommit [options]'
+  End
+
+  It 'fails with error message for unknown option'
+    When call ./aicommit --foo
+    The status should be failure
+    The stderr should include "unknown option '--foo'"
+    The stderr should include "Try 'aicommit --help' for more information."
   End
 End
