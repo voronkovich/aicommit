e2a9b1e65bfa2dae48bf1531cbfafbe76a043622
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2026-04-02 01:36:58 +0300

    test(spec): update commands to invoke aicommit without ./ and set PATH

--- spec/aicommit_spec.sh
+++ spec/aicommit_spec.sh
@@ -1,18 +1,20 @@
 Describe 'aicommit'
+  BeforeAll PATH="${PWD}:$PATH"
+
   It 'displays help message for -h'
-    When call ./aicommit --help
+    When call aicommit --help
     The status should be success
     The output should include 'Usage: aicommit [options]'
   End
 
   It 'displays help message for --help'
-    When call ./aicommit -h
+    When call aicommit -h
     The status should be success
     The output should include 'Usage: aicommit [options]'
   End
 
   It 'fails with error message for unknown option'
-    When call ./aicommit --foo
+    When call aicommit --foo
     The status should be failure
     The stderr should include "unknown option '--foo'"
     The stderr should include "Try 'aicommit --help' for more information."
